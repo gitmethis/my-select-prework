9f9f5f7efea6670ea4d521d1b40bbfa844ce0e34
Chase <chaseit12@gmail.com>
2018-02-20 00:52:11 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,9 +3,7 @@ def my_select(collection)
  new_arr = []
  i = 0
  while i < collection.length
-    if yield collection[i] == true
-      new_arr.push(collection[i])  
-    end
+    print yield collection[i]
     i+=1
  end
  new_arr
